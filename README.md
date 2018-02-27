c90523855102f3d84cb98f7260103be4f15b6de6
drSwobodziczka <drSwobodziczka@gmail.com>
2018-02-27 22:04:00 +0100

    - added browserified example - included running exercises section into readme

--- README.md
+++ README.md
@@ -36,3 +36,21 @@ https://yarnpkg.com/lang/en/
 ### Automation tool
 
 GULP --- TBD
+
+### Running exercises
+
+To run new exerices
+
+0. install simple http server for serving static content. ex.:
+
+    https://github.com/indexzero/http-server
+
+1. develop your scene deriving .js file in /lin
+2. bundle it with browserify
+3. create new .html file in `./public` reusing bundled scene
+4. run 
+
+``http-server [path] [options]``
+
+and browse your content 
+ 
--- public/bundle.js
+++ public/bundle.js
@@ -1,33 +1,26 @@
 (function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
-
 const THREE = require('three');
 
-function animate(cube) {
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+camera.position.z = 5;
+
+function animate() {
     requestAnimationFrame( animate );
     cube.rotation.x += 0.1;
     cube.rotation.y += 0.1;
     renderer.render( scene, camera );
 }
-
-module.exports = {
-    scene() {
-        const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-        const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-        const cube = new THREE.Mesh( geometry, material );
-        const scene = new THREE.Scene();
-        scene.add( cube );
-    },
-    renderer() {
-        const renderer = new THREE.WebGLRenderer();
-        renderer.setSize( window.innerWidth, window.innerHeight );
-    },
-    camera() {
-        const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-        camera.position.z = 5;
-    }
-
-};
-
+animate();
 },{"three":2}],2:[function(require,module,exports){
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
